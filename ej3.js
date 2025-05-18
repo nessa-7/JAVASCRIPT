93157761efd23c701531deeaf896062343693e6d
nessa <yisel7948@gmail.com>
2025-05-17 20:16:27 -0500

    Eliminado archivo/carpeta innecesaria

--- ej3.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")()
-
-let radio = parseInt(prompt("Ingrese el radio del circulo: "))
-
-function areacirculo(radio){
-    return Math.PI * Math.pow(radio,2)
-}
-
-console.log(areacirculo(radio))
